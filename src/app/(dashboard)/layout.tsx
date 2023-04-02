23801e556693e43935756d550aebc53e03ce0e75
rsbruno <rsbruno.cdc@gmail.com>
2023-04-02 10:59:11 -0300

    feat:app directory

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Next.js',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  )
+}
